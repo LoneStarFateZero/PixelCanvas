a4ca095a79a9afa2ec73231459f58173a4e3a475
LoneStarFateZero <1589186895@qq.com>
2019-04-09 00:12:38 +0800

    实现左侧导航栏收藏列表功能

--- app/src/main/java/pers/lonestar/pixelcanvas/activity/FavoriteActivity.java
+++ app/src/main/java/pers/lonestar/pixelcanvas/activity/FavoriteActivity.java
@@ -1,14 +1,136 @@
 package pers.lonestar.pixelcanvas.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import pers.lonestar.pixelcanvas.R;
+import pers.lonestar.pixelcanvas.adapter.FavoriteAdapter;
+import pers.lonestar.pixelcanvas.infostore.CanvasFavorite;
+import pers.lonestar.pixelcanvas.infostore.PixelUser;
+import pers.lonestar.pixelcanvas.listener.FavoriteRecyclerOnScrollListener;
 
 public class FavoriteActivity extends AppCompatActivity {
+    private static FavoriteActivity instance;
+    private RecyclerView recyclerView;
+    private FavoriteAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Toolbar toolbar;
+    private PixelUser pixelUser;
+    private List<CanvasFavorite> favoriteList;
+    //用于查询收藏列表
+    private BmobQuery<CanvasFavorite> loadMoreFavoriteQuery;
+    private int pageLimit = 10;
+    private int querySkip = 0;
+
+    public static FavoriteActivity getInstance() {
+        return instance;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        instance = this;
+
+        initView();
+        refreshData();
+    }
+
+    private void initView() {
+        pixelUser = BmobUser.getCurrentUser(PixelUser.class);
+
+        toolbar = findViewById(R.id.favorite_activity_toolbar);
+        toolbar.setTitle("收藏列表");
+        toolbar.setTitleTextAppearance(this, R.style.TitleStyle);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        swipeRefreshLayout = findViewById(R.id.favorite_activity_SwipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+
+        recyclerView = findViewById(R.id.favorite_activity_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        favoriteList = new ArrayList<>();
+        adapter = new FavoriteAdapter(favoriteList);
+        recyclerView.setAdapter(adapter);
+        //设置缓存
+        recyclerView.setItemViewCacheSize(50);
+        //设置子项布局大小不变，省去重新测量过程，提升性能
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addOnScrollListener(new FavoriteRecyclerOnScrollListener() {
+            @Override
+            public void onLoadMore() {
+                //获取更多数据
+                loadMoreData();
+            }
+        });
+    }
+
+    private void refreshData() {
+        loadMoreFavoriteQuery = new BmobQuery<>();
+        loadMoreFavoriteQuery.order("-createdAt");
+        loadMoreFavoriteQuery.include("canvas,creator");
+        loadMoreFavoriteQuery.addWhereEqualTo("favoriteUser", pixelUser);
+        loadMoreFavoriteQuery.findObjects(new FindListener<CanvasFavorite>() {
+            @Override
+            public void done(List<CanvasFavorite> list, BmobException e) {
+                swipeRefreshLayout.setRefreshing(false);
+                if (e == null) {
+                    favoriteList.clear();
+                    favoriteList.addAll(list);
+                    adapter.notifyDataSetChanged();
+                    if (list.isEmpty())
+                        adapter.setLoadState(adapter.LOADING_END);
+                    else {
+                        adapter.setLoadState(adapter.LOADING_COMPLETE);
+                    }
+                } else
+                    Toast.makeText(FavoriteActivity.this, "收藏列表获取失败，请检查网络设置", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadMoreData() {
+        querySkip += pageLimit;
+        adapter.setLoadState(adapter.LOADING);
+        loadMoreFavoriteQuery.setSkip(querySkip);
+        loadMoreFavoriteQuery.findObjects(new FindListener<CanvasFavorite>() {
+            @Override
+            public void done(List<CanvasFavorite> list, BmobException e) {
+                swipeRefreshLayout.setRefreshing(false);
+                if (e == null) {
+                    if (list.isEmpty()) {
+                        adapter.setLoadState(adapter.LOADING_END);
+                    } else {
+                        favoriteList.addAll(list);
+                        adapter.notifyDataSetChanged();
+                        adapter.setLoadState(adapter.LOADING_COMPLETE);
+                    }
+                } else
+                    Toast.makeText(FavoriteActivity.this, "收藏列表获取失败，请检查网络设置", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/pers/lonestar/pixelcanvas/adapter/FavoriteAdapter.java
@@ -0,0 +1,179 @@
+package pers.lonestar.pixelcanvas.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import de.hdodenhof.circleimageview.CircleImageView;
+import pers.lonestar.pixelcanvas.R;
+import pers.lonestar.pixelcanvas.activity.CanvasInfoActivity;
+import pers.lonestar.pixelcanvas.activity.FavoriteActivity;
+import pers.lonestar.pixelcanvas.activity.ProfileActivity;
+import pers.lonestar.pixelcanvas.infostore.CanvasFavorite;
+import pers.lonestar.pixelcanvas.utils.ParameterUtils;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    // 正在加载
+    public final int LOADING = 1;
+    // 加载完成
+    public final int LOADING_COMPLETE = 2;
+    // 加载到底
+    public final int LOADING_END = 3;
+    // 普通布局
+    private final int TYPE_ITEM = 1;
+    // 脚布局
+    private final int TYPE_FOOTER = 2;
+    // 当前加载状态，默认为加载完成
+    private int loadState = 2;
+
+    private List<CanvasFavorite> canvasFavoriteList;
+
+    public FavoriteAdapter(List<CanvasFavorite> canvasFavoriteList) {
+        super();
+        this.canvasFavoriteList = canvasFavoriteList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // 最后一个item设置为FooterView
+        if (position + 1 == getItemCount())
+            return TYPE_FOOTER;
+        else
+            return TYPE_ITEM;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //添加一般View
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.follow_canvas_item, parent, false);
+            return new FavoriteAdapter.RecyclerViewHolder(view);
+        }
+        //添加FootView
+        else if (viewType == TYPE_FOOTER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.footview, parent, false);
+            return new FavoriteAdapter.FootViewHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof FavoriteAdapter.RecyclerViewHolder) {
+            final FavoriteAdapter.RecyclerViewHolder recyclerViewHolder = (FavoriteAdapter.RecyclerViewHolder) holder;
+            final CanvasFavorite canvasFavorite = canvasFavoriteList.get(position);
+            //作品名称
+            recyclerViewHolder.canvasName.setText(canvasFavorite.getCanvas().getCanvasName());
+            //设置当前昵称
+            recyclerViewHolder.nickName.setText(canvasFavorite.getCreator().getNickname());
+            //设置发布时间
+            recyclerViewHolder.createdTime.setText(canvasFavorite.getCanvas().getCreatedAt());
+            //缩略图
+            Glide.with(FavoriteActivity.getInstance()).load(ParameterUtils.bytesToBitmap(canvasFavorite.getCanvas().getThumbnail())).into(recyclerViewHolder.thumbnail);
+            //设置当前头像
+            Glide.with(FavoriteActivity.getInstance()).load(canvasFavorite.getCreator().getAvatarUrl()).into(recyclerViewHolder.avatar);
+            //点击头像转到个人主页
+            recyclerViewHolder.avatar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(FavoriteActivity.getInstance(), ProfileActivity.class);
+                    intent.putExtra("pixel_user", canvasFavorite.getCreator());
+                    FavoriteActivity.getInstance().startActivity(intent);
+                }
+            });
+            //点击缩略图转到作品主页
+            recyclerViewHolder.thumbnail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(FavoriteActivity.getInstance(), CanvasInfoActivity.class);
+                    intent.putExtra("pixel_canvas", canvasFavorite.getCanvas());
+                    intent.putExtra("pixel_user", canvasFavorite.getCreator());
+                    FavoriteActivity.getInstance().startActivity(intent);
+                }
+            });
+        } else if (holder instanceof FavoriteAdapter.FootViewHolder) {
+            FavoriteAdapter.FootViewHolder footViewHolder = (FavoriteAdapter.FootViewHolder) holder;
+            switch (loadState) {
+                case LOADING: // 正在加载
+                    footViewHolder.pbLoading.setVisibility(View.VISIBLE);
+                    footViewHolder.tvLoading.setVisibility(View.VISIBLE);
+                    footViewHolder.llEnd.setVisibility(View.GONE);
+                    break;
+
+                case LOADING_COMPLETE: // 加载完成
+                    footViewHolder.pbLoading.setVisibility(View.INVISIBLE);
+                    footViewHolder.tvLoading.setVisibility(View.INVISIBLE);
+                    footViewHolder.llEnd.setVisibility(View.GONE);
+                    break;
+
+                case LOADING_END: // 加载到底
+                    footViewHolder.pbLoading.setVisibility(View.GONE);
+                    footViewHolder.tvLoading.setVisibility(View.GONE);
+                    footViewHolder.llEnd.setVisibility(View.VISIBLE);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return canvasFavoriteList.size() + 1;
+    }
+
+    /**
+     * 设置上拉加载状态
+     *
+     * @param loadState 0.正在加载 1.加载完成 2.加载到底
+     */
+    public void setLoadState(int loadState) {
+        if (this.loadState != LOADING_END) {
+            this.loadState = loadState;
+            notifyItemChanged(canvasFavoriteList.size());
+        }
+    }
+
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView avatar;
+        TextView nickName;
+        TextView createdTime;
+        TextView canvasName;
+        ImageView thumbnail;
+
+        RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            avatar = itemView.findViewById(R.id.follow_canvas_avatar);
+            nickName = itemView.findViewById(R.id.follow_canvas_nickname);
+            createdTime = itemView.findViewById(R.id.follow_canvas_time);
+            canvasName = itemView.findViewById(R.id.follow_canvas_name);
+            thumbnail = itemView.findViewById(R.id.follow_canvas_thumbnail);
+        }
+    }
+
+    private class FootViewHolder extends RecyclerView.ViewHolder {
+        ProgressBar pbLoading;
+        TextView tvLoading;
+        LinearLayout llEnd;
+
+        FootViewHolder(@NonNull View itemView) {
+            super(itemView);
+            pbLoading = itemView.findViewById(R.id.pb_loading);
+            tvLoading = itemView.findViewById(R.id.tv_loading);
+            llEnd = itemView.findViewById(R.id.ll_end);
+        }
+    }
+}
